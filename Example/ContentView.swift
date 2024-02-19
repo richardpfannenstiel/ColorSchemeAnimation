a28f0137d54d8b954ea23e71bd23759e2baf2b78
Richard <57414694+richardpfannenstiel@users.noreply.github.com>
2024-02-19 17:13:43 +0100

    Add example app

--- Example/ContentView.swift
+++ Example/ContentView.swift
@@ -1,18 +1,71 @@
 //
-//  SwiftUIView.swift
-//  
+//  ContentView.swift
+//
 //
 //  Created by Richard Pfannenstiel on 19.02.24.
 //
 
 import SwiftUI
+import ColorSchemeAnimation
 
-struct SwiftUIView: View {
+struct ContentView: View {
+    
+    @Environment(\.customColorScheme) var colorScheme
+    
+    let colorSchemes: [CustomColorScheme] = [.system, .light, .dark]
+    
+    @State var animationDuration: TimeInterval = 1
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                Section {
+                    Text("You're using the \(colorScheme.rawValue) color scheme.")
+                } header: {
+                    Text("Color Scheme Display")
+                }
+                Section {
+                    HStack(spacing: 20) {
+                        ForEach(colorSchemes, id: \.self) { colorScheme in
+                            Text(colorScheme.rawValue)
+                                .frame(width: 100)
+                                .padding(.vertical, 5)
+                                .background(.secondary.opacity(0.3))
+                                .cornerRadius(10)
+                                .onTapChangeColorScheme(to: colorScheme)
+                        }
+                    }
+                } header: {
+                    Text("Distinct Buttons")
+                }
+                Section {
+                    VStack {
+                        Slider(value: $animationDuration, in: 0.5...10, step: 0.5)
+                        Text("Animation Duration: \(animationDuration, specifier: "%.1f")s")
+                    }
+                    HStack {
+                        Spacer()
+                        Image(systemName: colorScheme == .dark ? "lightbulb.max.fill" : "lightbulb.slash.fill")
+                        Text(colorScheme == .dark ? "Turn on the lights" : "Turn off the lights")
+                            .padding(.vertical, 5)
+                        Spacer()
+                    }
+                    .onTapChangeColorScheme(to: colorScheme == .dark ? .light : .dark, animationDuration: animationDuration)
+                    .background(.secondary.opacity(0.3))
+                    .cornerRadius(10)
+                } header: {
+                    Text("Animation Speed")
+                }
+            }.navigationTitle("Settings")
+                .toolbar(content: {
+                    Image(systemName: colorScheme == .dark ? "sun.max.fill" : "moon.fill")
+                        .onTapChangeColorScheme(to: colorScheme == .dark ? .light : .dark)
+                })
+        }
     }
 }
 
 #Preview {
-    SwiftUIView()
+    ContentView()
+        .preferredColorScheme()
 }
--- Example/ExampleApp.swift
+++ Example/ExampleApp.swift
@@ -1,18 +1,18 @@
 //
-//  SwiftUIView.swift
-//  
+//  ExampleApp.swift
+//
 //
 //  Created by Richard Pfannenstiel on 19.02.24.
 //
 
 import SwiftUI
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme()
+        }
     }
-}
-
-#Preview {
-    SwiftUIView()
 }
